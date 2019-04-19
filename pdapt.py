ee380f00fd34c690aca769cd3b1f78319127edad
Maxwell Hunter <MaxwellHunter@gmail.com>
2019-04-19 02:00:04 -0700

    added documentation

--- pdapt.py
+++ pdapt.py
@@ -9,6 +9,7 @@ from flask_caching import Cache
 
 import config
 import models
+# all the action happens here
 from resources.guid import mdObj_api
 
 # initialize the database
@@ -22,12 +23,17 @@ app.register_blueprint(mdObj_api)
 cache = Cache(app, config={'CACHE_TYPE': 'redis'})
 cache.init_app(app)
 
-# add the index route to confirm the API is active
+
+# add the index route to confirm the API is active,
+# all the action happens in resources/guid.py
 @app.route('/')
 def my_guids():
     return "REST API Active"
 
 
 if __name__ == '__main__':
+    '''
+    Runs the api server if name is main
+    '''
     app.run(debug=config.DEBUG, host=config.HOST,
             port=config.PORT)  # pragma: no cover
